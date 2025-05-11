5b6db8e99373f1d588440a99a70d67f8d45c1014
chrstphfrtz <christoph.fritz@chrstphfrtz.com>
2025-05-11 18:29:40 +0200

    a

--- backend/packages/functions/todos/src/index.ts
+++ backend/packages/functions/todos/src/index.ts
@@ -19,23 +19,23 @@ interface DigitalOceanResponse {
 
 export async function main(event: DigitalOceanEvent, context: any): Promise<DigitalOceanResponse> {
     if (event.http && event.http.method === 'GET') {
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-        });
-        try {
-            await client.connect();
-            console.log('Connected to the database successfully!');
+        // const client = new Client({
+        //     connectionString: process.env.DATABASE_URL,
+        // });
+        // try {
+        //     await client.connect();
+        //     console.log('Connected to the database successfully!');
 
-            // You can now execute queries
-            // const result = await client.query('SELECT NOW()');
-            // console.log('Database time:', result.rows[0].now);
+        //     // You can now execute queries
+        //     // const result = await client.query('SELECT NOW()');
+        //     // console.log('Database time:', result.rows[0].now);
 
-        } catch (err) {
-            console.error('Error connecting to the database:', err);
-        } finally {
-            await client.end(); // Close the connection when done
-            console.log('Database connection closed.');
-        }
+        // } catch (err) {
+        //     console.error('Error connecting to the database:', err);
+        // } finally {
+        //     await client.end(); // Close the connection when done
+        //     console.log('Database connection closed.');
+        // }
         // // Access query parameters.
         // // DigitalOcean often puts parsed query params at the top level of the event.
         // const name = event.name as string | undefined || 'stranger';
@@ -49,7 +49,7 @@ export async function main(event: DigitalOceanEvent, context: any): Promise<Digi
 
         return {
             statusCode: 200,
-            body: "testeroni",
+            body: process.env.DATABASE_URL,
             headers: {
                 'Content-Type': 'text/plain'
             }
