2be6767d28cd35006fcfa9510744d416a63e6748
chrstphfrtz <christoph.fritz@chrstphfrtz.com>
2025-05-11 14:55:20 +0200

    a

---
+++ backend/packages/functions/todos/src/index.ts
@@ -0,0 +1,54 @@
+import { Pool, PoolClient } from 'pg';
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('error', (err: Error) => {
+    console.error('Unexpected error on idle client', err);
+});
+
+interface DigitalOceanEvent {
+    http?: {
+        method: string;
+        path: string;
+        headers: { [key: string]: string };
+        queryString: string;
+        body?: any;
+    };
+    [key: string]: any;
+}
+
+interface DigitalOceanResponse {
+    statusCode: number;
+    body: any;
+    headers?: { [key: string]: string };
+}
+
+export async function main(event: DigitalOceanEvent, context: any): Promise<DigitalOceanResponse> {
+    if (event.http && event.http.method === 'GET') {
+        // // Access query parameters.
+        // // DigitalOcean often puts parsed query params at the top level of the event.
+        // const name = event.name as string | undefined || 'stranger';
+        // const greeting = event.greeting as string | undefined;
+
+        // let message = `Hello, ${name}! This was a GET request.`;
+
+        // if (greeting) {
+        //     message = `${greeting}, ${name}! This was a GET request.`;
+        // }
+
+        return {
+            statusCode: 200,
+            body: "testeroni",
+            headers: {
+                'Content-Type': 'text/plain'
+            }
+        };
+    } else {
+        return {
+            statusCode: 405,
+            body: 'Only GET requests are allowed.'
+        };
+    }
+}
---
+++ iac/index.ts
@@ -0,0 +1,35 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as digitalocean from "@pulumi/digitalocean";
+
+const demoDatabaseCluster = new digitalocean.DatabaseCluster("demoDatabaseCluster", {
+  nodeCount: 1,
+  engine: "pg",
+  size: digitalocean.DatabaseSlug.DB_1VPCU1GB,
+  region: digitalocean.Region.FRA1,
+  name: "main",
+  projectId: "c1cf44a6-cd57-41bf-b95b-f6a60efed99c",
+  version: "17",
+});
+
+const demoFunction = new digitalocean.App("demoFunction", {
+  projectId: "c1cf44a6-cd57-41bf-b95b-f6a60efed99c",
+  spec: {
+    name: "demo-function",
+    functions: [{
+      name: "todos",
+      github: {
+        repo: "chrstphfrtz/how-iac-can-help-you-speed-up-development-and-testing-demo",
+        branch: "main"
+      },
+      sourceDir: "backend"
+    }]
+  }
+})
+
+const demoFirewall = new digitalocean.DatabaseFirewall("demoFirewall", {
+  clusterId: demoDatabaseCluster.id,
+  rules: [{
+    type: "app",
+    value: demoFunction.id
+  }]
+})
